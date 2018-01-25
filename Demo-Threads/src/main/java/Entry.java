252cb7764bf7ece43f6dbfd44a8ec2c37d7865e5
abhishek-ghode <abhishek.ghode@capgemini.com>
2018-01-25 14:12:56 +0800

    Modified Ebtry.java to use Runnable

--- Demo-Threads/src/main/java/Entry.java
+++ Demo-Threads/src/main/java/Entry.java
@@ -2,65 +2,63 @@
 public class Entry {
 
 	public static void main(String[] args) {
-		
+
 		String currentThread = Thread.currentThread().getName();
-		
+
 		System.out.println(currentThread);
-		
-		Thread t1 = new MyThread();
+
+		Thread t1;
+
+		Runnable target = new MyJob();
+
+		// t1 = new MyThread();
+		t1 = new Thread(target, "T1");
+
 		t1.start();
-		
+
 		Thread t2 = new MyAnotherThread();
 		t2.start();
-		
-		
+
 	}
-	
+
 }
 
-class MyAnotherThread extends Thread{
-	
+class MyAnotherThread extends Thread {
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-//		super.run();
-		
+		// super.run();
+
 		int count = 0;
 		try {
-			while(true){
+			while (true) {
 				Thread.sleep(900);
-				System.out.println((count += 2) + " in "+ Thread.currentThread().getName());
+				System.out.println((count += 2) + " in " + Thread.currentThread().getName());
 			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 }
 
-
-class MyThread extends Thread{
+class MyJob implements Runnable {
 	@Override
-	public void run() /*throws InterruptedException */{
+	public void run() /* throws InterruptedException */ {
 		// TODO Auto-generated method stub
-		super.run();
-		System.out.println("inside run()...");
-		
+		// super.run();
+
 		int count = 1;
-		while(true){
+		while (true) {
 			try {
 				Thread.sleep(700);
-				System.out.println((count += 2) + " in "+ Thread.currentThread().getName());
+				System.out.println((count += 2) + " in " + Thread.currentThread().getName());
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-		
-		
-		
-		
-	}
-	
-}
 
+	}
+
+}
